0a9e1f244e916038db22c05fda98d7262f09e6fa
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2023-10-05 22:39:06 +0600

    dynamic date-time added with moment-js

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
-
+import logo from "../../src/assets/logo.png"
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h1>This is Header</h1>
+            <img className="my-0 mx-auto" src={logo}></img>
+            <p className="text-center mt-3 md:mt-5 text-xs md:text-base font-normal text-[#706F6F]">Journalism Without Fear or Favour</p>
+            <p className="mt-3 md:mt-4 text-center text-[#403F3F] text-sm md:text-xl font-medium">{moment().format("dddd, ")}<span className="text-[#706F6F] text-sm md:text-xl font-medium">{moment().format("MMMM D, YYYY")}</span></p>
             
         </div>
     );
